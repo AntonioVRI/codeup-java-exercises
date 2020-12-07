b7b55926f547f41a1aa4eb4eb3db5b88751fdbc6
Anthony V <avidaurri4@gmail.com>
2020-12-06 23:59:14 -0600

    methods exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,10 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+table(3);
+    }
+//public static void table(int number){
+//for(int i=1;i<=10;i++){
+//    System.out.format("%d * %d = %d \n",number,i,number*i);
+//    }
+//}
+}
