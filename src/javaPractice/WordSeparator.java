023a1e9bb5196f5be67f063a8463d29e6c6b82cf
Anthony V <avidaurri4@gmail.com>
2021-01-05 23:54:58 -0600

    working on wordseparator

---
+++ src/javaPractice/WordSeparator.java
@@ -0,0 +1,31 @@
+package javaPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class WordSeparator {
+
+	public static void main(String[] args) {
+
+		// Create a Scanner object for keyboard input.
+		Scanner keyboard = new Scanner(System.in);
+
+		// Get a string from the user.
+		System.out.println("Enter a sentence with no spaces "
+				+ "between the words and each " + "word is capitalized.");
+		System.out.println("An example is: StopAndSmellTheRoses");
+		System.out.print("> ");
+
+		// get user input
+		String userInput = keyboard.nextLine();
+
+		// close keyboard
+		keyboard.close();
+
+		// need new array list to format
+	}
+
+}
