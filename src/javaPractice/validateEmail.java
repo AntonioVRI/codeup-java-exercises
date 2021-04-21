ea46a84019a7f62bcbb853e221693c57c6c6a4cc
Anthony V <avidaurri4@gmail.com>
2021-04-20 23:48:13 -0500

    emailvalidation

---
+++ src/javaPractice/validateEmail.java
@@ -0,0 +1,10 @@
+package javaPractice;
+
+public class validateEmail {
+    public static boolean validateEmail(String s) {
+        boolean result = false, at = false, dot = false;
+        int atPos = -1;
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+}
