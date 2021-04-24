32126bdfbf565c9cbbdb68939e74ad6f2e65ba33
Anthony V <avidaurri4@gmail.com>
2021-04-23 22:08:28 -0500

    main

--- src/javaPractice/validateEmail.java
+++ src/javaPractice/validateEmail.java
@@ -5,6 +5,16 @@ public class validateEmail {
         boolean result = false, at = false, dot = false;
         int atPos = -1;
         for (int i = 0; i < s.length(); i++) {
-
+            if (s.charAt(i) == '@' && i > 0 && i < s.length() - 3) {
+                at = true;
+                atPos = i;
+            }
+            if (s.charAt(i) == '.' && atPos > 0 && i > atPos && i < s.length() - 1) {
+                dot = true;
+            }
         }
+        result = at && dot;
+        return result;
+    }
+
 }
