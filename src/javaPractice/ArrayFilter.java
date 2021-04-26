1b4510eec71c5c87597d3ebfec957ec61b022ddf
Anthony V <avidaurri4@gmail.com>
2021-04-25 23:27:27 -0500

    arrayfilter practice

---
+++ src/javaPractice/ArrayFilter.java
@@ -0,0 +1,23 @@
+package javaPractice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class ArrayFilter {
+    public static String[] filterArray(String[] str) {
+        List<String> list = new ArrayList<String>();
+        Set<String> unique = new HashSet<String>();
+
+        for (int i = 0; i < str.length; i++) {
+            String current = str[i];
+            Pattern p = Pattern.compile("\\D+");
+            Matcher m = p.matcher(current);
+
+
+    }
+}
