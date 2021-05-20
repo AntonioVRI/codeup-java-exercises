9361acb89ebe4716192b3335cd36c31c99e54d93
Anthony V <avidaurri4@gmail.com>
2021-05-19 23:35:49 -0500

    anagram practice problem

--- src/javaPractice/Anagrams.java
+++ src/javaPractice/Anagrams.java
@@ -5,23 +5,39 @@ import java.util.Scanner;
 
 public class Anagrams {
 
-    static boolean isAnagram(String firstWord, String secondWord) {
-        char[] word1 = firstWord.toLowerCase().replaceAll("[\\s]", "").toCharArray();
-        char[] word2 = secondWord.toLowerCase().replaceAll("[\\s]", "").toCharArray();
-        Arrays.sort(word1);
-        Arrays.sort(word2);
-        return Arrays.equals(word1, word2);
-    }
+    //    static boolean isAnagram(String firstWord, String secondWord) {
+//        char[] word1 = firstWord.toLowerCase().replaceAll("[\\s]", "").toCharArray();
+//        char[] word2 = secondWord.toLowerCase().replaceAll("[\\s]", "").toCharArray();
+//        Arrays.sort(word1);
+//        Arrays.sort(word2);
+//        return Arrays.equals(word1, word2);
+//    }
+//
+//
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc=new Scanner(System.in);
+//        String a=sc.next();
+//        String b=sc.next();
+//        boolean ret=isAnagram(a,b);
+//        if(ret)System.out.println("Anagrams");
+//        else System.out.println("Not Anagrams");
+//    }
+//}
+    private static int[] frequency = new int[26];
 
+    static boolean isAnagram(String a, String b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
 
+        for (char c : a.toCharArray()) {
+            frequency[(int) c - 97]++;
+        }
+        for (char c : b.toCharArray()) {
+            frequency[(int) c - 97]--;
+        }
 
-    public static void main(String[] args) {
-
-        Scanner sc=new Scanner(System.in);
-        String a=sc.next();
-        String b=sc.next();
-        boolean ret=isAnagram(a,b);
-        if(ret)System.out.println("Anagrams");
-        else System.out.println("Not Anagrams");
+        return anagrams;
     }
 }
