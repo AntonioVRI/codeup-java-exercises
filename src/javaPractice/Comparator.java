a1d0265146fe7b511b70f3426d750c10e9a18e43
Anthony V <avidaurri4@gmail.com>
2021-06-07 23:32:44 -0500

    comparator

--- src/javaPractice/Comparator.java
+++ src/javaPractice/Comparator.java
@@ -30,18 +30,23 @@ public class Comparator {
         }
     }
 
-    class StudentComparator implements Comparator<Student> {
-        @Override
-        public int compare(Student o1, Student o2) {
-            if (o1.getCGPA() == o2.getCGPA()) {
-                if(o1.getName().equals(o2.getName())) {
-                    return Integer.compare(o2.getID(), o1.getID());
-                } else {
-                    return o1.getName().compareTo(o2.getName());
-                }
-            }
-            return Double.compare(o2.getCGPA(), o1.getCGPA());
-        }
-    }
+//    class StudentComparator implements Comparator<Student> {
+//        @Override
+//        public int compare(Student o1, Student o2) {
+//            if (o1.getCGPA() == o2.getCGPA()) {
+//                if(o1.getName().equals(o2.getName())) {
+//                    return Integer.compare(o2.getID(), o1.getID());
+//                } else {
+//                    return o1.getName().compareTo(o2.getName());
+//                }
+//            }
+//            return Double.compare(o2.getCGPA(), o1.getCGPA());
+//        }
+//    }
+
+    class Priorities {
+        public List<Student> getStudents(List<String> events) {
+            PriorityQueue pq = new PriorityQueue<>(events.size(), new StudentComparator());
+
 
 }
