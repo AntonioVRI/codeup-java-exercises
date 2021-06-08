0125197c7d30009d93bf3a47f7cddbc9d3af91ad
Anthony V <avidaurri4@gmail.com>
2021-06-07 23:41:56 -0500

    practice comparator;

--- src/javaPractice/Comparator.java
+++ src/javaPractice/Comparator.java
@@ -30,23 +30,43 @@ public class Comparator {
         }
     }
 
-//    class StudentComparator implements Comparator<Student> {
-//        @Override
-//        public int compare(Student o1, Student o2) {
-//            if (o1.getCGPA() == o2.getCGPA()) {
-//                if(o1.getName().equals(o2.getName())) {
-//                    return Integer.compare(o2.getID(), o1.getID());
-//                } else {
-//                    return o1.getName().compareTo(o2.getName());
-//                }
-//            }
-//            return Double.compare(o2.getCGPA(), o1.getCGPA());
-//        }
-//    }
+    class StudentComparator implements Comparator<Student> {
+        @Override
+        public int compare(Student o1, Student o2) {
+            if (o1.getCGPA() == o2.getCGPA()) {
+                if(o1.getName().equals(o2.getName())) {
+                    return Integer.compare(o2.getID(), o1.getID());
+                } else {
+                    return o1.getName().compareTo(o2.getName());
+                }
+            }
+            return Double.compare(o2.getCGPA(), o1.getCGPA());
+        }
+    }
 
     class Priorities {
         public List<Student> getStudents(List<String> events) {
             PriorityQueue pq = new PriorityQueue<>(events.size(), new StudentComparator());
 
+            for (String event: events) {
+                String parts[] = event.split(" ");
 
+                if (parts[0].equals("ENTER")) {
+                    Student stu = new Student(Integer.parseInt(parts[3]), parts[1], Double.parseDouble(parts[2]));
+                    pq.add(stu);
+                } else {
+                    if (!pq.isEmpty()) {
+                        pq.poll();
+                    }
+                }
+            }
+
+            List<Student> students = new ArrayList<>();
+            while (!pq.isEmpty()) {
+                students.add((Student) pq.poll());
+            }
+
+            return students;
+        }
+    }
 }
