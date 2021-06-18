e5316733a5ae54e8e596badaa4638ac6f1c647b3
Anthony V <avidaurri4@gmail.com>
2021-06-17 23:39:07 -0500

    fibonacci sequence

--- src/javaPractice/FizzBuzz.java
+++ src/javaPractice/FizzBuzz.java
@@ -1,31 +1,28 @@
 package javaPractice;
 import java.util.*;
-class FizzBuzz
-{
-    public static void main(String args[])
-    {
+class FizzBuzz {
+    public static void main(String args[]) {
         int n = 100;
 
         // loop for 100 times
-        for (int i=1; i<=n; i++)
-        {
+        for (int i = 1; i <= n; i++) {
             //number divisible by 15(divisible by
             // both 3 & 5), print 'FizzBuzz' in
             // place of the number
-            if (i%15==0)
-                System.out.print("FizzBuzz"+" ");
+            if (i % 15 == 0)
+                System.out.print("FizzBuzz" + " ");
                 // number divisible by 5, print 'Buzz'
                 // in place of the number
-            else if (i%5==0)
-                System.out.print("Buzz"+" ");
+            else if (i % 5 == 0)
+                System.out.print("Buzz" + " ");
 
                 // number divisible by 3, print 'Fizz'
                 // in place of the number
-            else if (i%3==0)
-                System.out.print("Fizz"+" ");
+            else if (i % 3 == 0)
+                System.out.print("Fizz" + " ");
 
             else // print the numbers
-                System.out.print(i+" ");
+                System.out.print(i + " ");
         }
     }
 
@@ -39,5 +36,15 @@ class FizzBuzz
             }
         }
         return isPrime;
+    }
+
+    public Integer fibonacci(Integer n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 0) {
+            return 0;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
     }
 }
