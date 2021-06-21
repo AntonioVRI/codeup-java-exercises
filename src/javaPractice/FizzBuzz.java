6f28a006a61799bbc54a648ae3c9fef78ebf6ac9
Anthony V <avidaurri4@gmail.com>
2021-06-20 23:38:47 -0500

    cypher

--- src/javaPractice/FizzBuzz.java
+++ src/javaPractice/FizzBuzz.java
@@ -47,4 +47,16 @@ class FizzBuzz {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
+
+    public String decode(String code) {
+        String decoded = "";
+        int lastCharValue = 'z';
+        int alphabetLength = 'z' - 'a' + 1;
+//        for (char character : code.toCharArray()) {
+//            int charNoRotation = character + 5;
+//            int charValue = charNoRotation < lastCharValue ? charNoRotation : charNoRotation - alphabetLength;
+//            decoded += (char) charValue;
+//        }
+        return decoded;
+    }
 }
