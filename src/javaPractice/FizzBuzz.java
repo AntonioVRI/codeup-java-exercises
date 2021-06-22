9665bdfd065a05f7d6ec5ff9de13d0a70ff94f2b
Anthony V <avidaurri4@gmail.com>
2021-06-21 23:42:48 -0500

    sumTarget

--- src/javaPractice/FizzBuzz.java
+++ src/javaPractice/FizzBuzz.java
@@ -59,4 +59,18 @@ class FizzBuzz {
 //        }
         return decoded;
     }
+
+    public Boolean sumsToTarget(Integer[] arr, Integer target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+            if (numMap.containsKey(complement)) {
+                return true;
+            } else {
+                numMap.put(arr[i], i);
+            }
+        }
+        return false;
+    }
+
 }
